663b4ad6e911db58fc4fe5bec3c0cb85c16ee14d
ARMANDO BUGARIN <armando.bugarin24@gmail.com>
2023-11-04 22:13:35 -0700

    multiplyArray test pass

--- js/app.js
+++ js/app.js
@@ -95,7 +95,7 @@ function sumArray(sumArr) { //eslint-disable-line
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -112,10 +112,16 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
 
+  let productPartialArray = multiply(multArr[0], multArr[1]);
+  let productAnswerArray = productPartialArray[0];
+
+  let productArrayNumber = multiply(productAnswerArray, multArr[2]);
+
+  return [productArrayNumber[0], `The numbers ${multArr[0]},${multArr[1]},${multArr[2]} have a product of ${productArrayNumber[0]}.`];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
